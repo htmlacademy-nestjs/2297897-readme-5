5ae252a5d17e4b4f843b225d0a1fe09e0a04fac2
focuzzed <1limbo.theorem1@gmail.com>
2023-12-01 18:01:24 +0300

    feat: add all microservices

--- project/apps/paginations/src/main.ts
+++ project/apps/paginations/src/main.ts
@@ -12,9 +12,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 8000;
+  const port = process.env.PORT || 6000;
   await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  );
 }
 
 bootstrap();
