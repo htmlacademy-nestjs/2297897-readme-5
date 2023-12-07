dc6457bc49e37c277190ccf75fb1f240aaad3764
focuzzed <1limbo.theorem1@gmail.com>
2023-12-07 16:57:41 +0300

    refactor: cond paginations, add actions

--- project/apps/actions/jest.config.ts
+++ project/apps/actions/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'paginations',
+  displayName: 'actions',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/paginations',
+  coverageDirectory: '../../coverage/apps/actions',
 };
