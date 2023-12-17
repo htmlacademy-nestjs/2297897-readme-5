66ec3a986b39309b4bf97039480a6c57257355df
focuzzed <1limbo.theorem1@gmail.com>
2023-12-17 03:02:31 +0300

    refactor: rename dbConfig, use confService in main dir

--- project/libs/shared/config/users/src/lib/config-users.module.ts
+++ project/libs/shared/config/users/src/lib/config-users.module.ts
@@ -2,14 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ENV_USERS_FILEPATH } from './config-users.const';
 import applicationConfig from './app.config';
-import databaseConfig from './database.config';
+import mongoConfig from './mongo.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       cache: true,
-      load: [applicationConfig, databaseConfig],
+      load: [applicationConfig, mongoConfig],
       envFilePath: ENV_USERS_FILEPATH,
     })
   ]
