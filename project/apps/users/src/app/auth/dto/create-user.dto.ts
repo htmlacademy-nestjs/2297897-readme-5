789bd8c76b2ee600aba0007d0be979caf0e7edb8
focuzzzed <1limbo.theorem1@gmail.com>
2024-01-16 21:49:27 +0300

    feat: add validation pipe to users MS, add users validation

--- project/apps/users/src/app/auth/dto/create-user.dto.ts
+++ project/apps/users/src/app/auth/dto/create-user.dto.ts
@@ -1,16 +1,29 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsEmail, IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
+import { USER_AVAILABLE_VALUE } from '../../user/user.constant';
+import { USER_VALIDATON_MESSAGE } from '../validation.messages';
 
 export class CreateUserDTO {
   @ApiProperty({
     description: 'User name',
     example: 'RonWizley'
   })
+  @IsString()
+  @MinLength(
+    USER_AVAILABLE_VALUE.NAME.MIN_LENGTH,
+    {message: USER_VALIDATON_MESSAGE.USER_NAME_NOT_VALID.MIN_LENGTH}
+  )
+  @MaxLength(
+    USER_AVAILABLE_VALUE.NAME.MAX_LENGTH,
+    {message: USER_VALIDATON_MESSAGE.USER_NAME_NOT_VALID.MAX_LENGTH}
+  )
   public name: string;
 
   @ApiProperty({
     description: 'User unique adress',
     example: 'user@local.com'
   })
+  @IsEmail({}, {message: USER_VALIDATON_MESSAGE.EMAIL_NOT_VALID})
   public email: string;
 
   @ApiPropertyOptional({
@@ -18,11 +31,22 @@ export class CreateUserDTO {
     example: 'path/to/user/image.jpeg',
 
   })
+  @IsOptional()
+  @IsString()
   public avatarUrl: string;
 
   @ApiProperty({
     description: 'User password',
     example: '31337'
   })
+  @IsString()
+  @MinLength(
+    USER_AVAILABLE_VALUE.PASSWORD.MIN_LENGTH,
+    {message: USER_VALIDATON_MESSAGE.PASSWORD_LENGTH_NOT_VALID.MIN_LENGTH}
+  )
+  @MaxLength(
+    USER_AVAILABLE_VALUE.PASSWORD.MAX_LENGTH,
+    {message: USER_VALIDATON_MESSAGE.PASSWORD_LENGTH_NOT_VALID.MAX_LENGTH}
+  )
   public password: string;
 }
