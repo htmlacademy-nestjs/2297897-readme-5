767c64e32d1be1844ba85a0db5270fab7087e6c5
focuzzzed <1limbo.theorem1@gmail.com>
2024-01-25 02:30:48 +0300

    feat: add file-uploader module

--- project/apps/file-vault/src/app/app.module.ts
+++ project/apps/file-vault/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
+import { FileUploaderModule } from './file-uploader/file-uploader.module';
+import { FileVaultConfigModule } from '@project/libs/shared/config/file-vault';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [FileUploaderModule, FileVaultConfigModule],
 })
 export class AppModule {}
