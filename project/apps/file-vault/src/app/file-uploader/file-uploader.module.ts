ec3869d1f23a2a0e9cf55432fda85315d2fa0a54
focuzzzed <1limbo.theorem1@gmail.com>
2024-01-25 12:12:30 +0300

    feat: connect ServeStaticModule to FileUploaderModule

--- project/apps/file-vault/src/app/file-uploader/file-uploader.module.ts
+++ project/apps/file-vault/src/app/file-uploader/file-uploader.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { FileUploaderController } from './file-uploader.controller';
 import { FileUploaderService } from './file-uploader.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { ConfigService } from '@nestjs/config';
+import { SERVE_ROOT } from './file-uploader.const';
 
 @Module({
+  imports: [ServeStaticModule.forRootAsync({
+    inject: [ConfigService],
+    useFactory: (configService: ConfigService) => {
+      const rootPath = configService.get<string>('application.uploadDirectory');
+      return [{
+        rootPath,
+        serveRoot: SERVE_ROOT,
+      }]
+    }
+  })],
   controllers: [FileUploaderController],
   providers: [FileUploaderService],
 })
