781162d7ce5a7596895a7aa1ac8db97985187c9b
focuzzzed <1limbo.theorem1@gmail.com>
2024-01-25 13:03:16 +0300

    feat: add mongo variables to config, create mongo factory, connect MongooseModule to file-vault root

--- project/apps/file-vault/src/app/app.module.ts
+++ project/apps/file-vault/src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { FileUploaderModule } from './file-uploader/file-uploader.module';
-import { FileVaultConfigModule } from '@project/libs/shared/config/file-vault';
+import { FileVaultConfigModule, getMongooseOptions } from '@project/libs/shared/config/file-vault';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [FileUploaderModule, FileVaultConfigModule],
+  imports: [
+    FileUploaderModule,
+    FileVaultConfigModule,
+    MongooseModule.forRootAsync(getMongooseOptions()),
+  ],
 })
 export class AppModule {}
--- project/libs/shared/config/file-vault/src/lib/file-vault.config.ts
+++ project/libs/shared/config/file-vault/src/lib/file-vault.config.ts
@@ -6,12 +6,28 @@ export interface FileVaultConfig {
   environment: Environment,
   port: number,
   uploadDirectory: string,
+  db: {
+    host: string,
+    port: number,
+    user: string,
+    name: string,
+    password: string,
+    authBase: string
+  }
 }
 
 const validationSchema = Joi.object({
   environment: Joi.string().required(),
   port: Joi.number().required(),
   uploadDirectory: Joi.string().required(),
+  db: Joi.object({
+    host: Joi.string().valid().hostname(),
+    port: Joi.number().port(),
+    name: Joi.string().required(),
+    user: Joi.string().required(),
+    password: Joi.string().required(),
+    authBase: Joi.string().required(),
+  })
 })
 
 function validateConfig(config: FileVaultConfig): void {
@@ -27,6 +43,14 @@ function getConfig(): FileVaultConfig {
     environment: process.env.NODE_ENV as Environment,
     port: parseInt(process.env.PORT, 10),
     uploadDirectory: process.env.UPLOAD_DIRECTORY_PATH,
+    db: {
+      host: process.env.MONGO_HOST,
+      port: parseInt(process.env.MONGO_PORT, 10),
+      name: process.env.MONGO_DB,
+      user: process.env.MONGO_USER,
+      password: process.env.MONGO_PASSWORD,
+      authBase: process.env.MONGO_AUTH_BASE,
+    }
   }
 
   validateConfig(config);
