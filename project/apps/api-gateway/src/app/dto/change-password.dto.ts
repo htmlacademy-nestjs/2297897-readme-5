92bd4d718d90425fb42e749f9fb59cc7edaf509f
focuzzzed <1limbo.theorem1@gmail.com>
2024-01-31 21:11:18 +0300

    feat: format files

--- project/apps/api-gateway/src/app/dto/change-password.dto.ts
+++ project/apps/api-gateway/src/app/dto/change-password.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { USER_VALIDATON_MESSAGE } from '../../../../users/src/app/user/validation.messages';
-import {IsString, MaxLength, MinLength } from 'class-validator';
+import { IsString, MaxLength, MinLength } from 'class-validator';
 import { USER_AVAILABLE_VALUE } from '../../../../users/src/app/user/user.constant';
 
 export class ChangePasswordDTO {
@@ -11,11 +11,11 @@ export class ChangePasswordDTO {
   @IsString()
   @MinLength(
     USER_AVAILABLE_VALUE.PASSWORD.MIN_LENGTH,
-    {message: USER_VALIDATON_MESSAGE.PASSWORD_LENGTH_NOT_VALID.MIN_LENGTH}
+    { message: USER_VALIDATON_MESSAGE.PASSWORD_LENGTH_NOT_VALID.MIN_LENGTH }
   )
   @MaxLength(
     USER_AVAILABLE_VALUE.PASSWORD.MAX_LENGTH,
-    {message: USER_VALIDATON_MESSAGE.PASSWORD_LENGTH_NOT_VALID.MAX_LENGTH}
+    { message: USER_VALIDATON_MESSAGE.PASSWORD_LENGTH_NOT_VALID.MAX_LENGTH }
   )
   oldPassword: string;
 
@@ -26,11 +26,11 @@ export class ChangePasswordDTO {
   @IsString()
   @MinLength(
     USER_AVAILABLE_VALUE.PASSWORD.MIN_LENGTH,
-    {message: USER_VALIDATON_MESSAGE.PASSWORD_LENGTH_NOT_VALID.MIN_LENGTH}
+    { message: USER_VALIDATON_MESSAGE.PASSWORD_LENGTH_NOT_VALID.MIN_LENGTH }
   )
   @MaxLength(
     USER_AVAILABLE_VALUE.PASSWORD.MAX_LENGTH,
-    {message: USER_VALIDATON_MESSAGE.PASSWORD_LENGTH_NOT_VALID.MAX_LENGTH}
+    { message: USER_VALIDATON_MESSAGE.PASSWORD_LENGTH_NOT_VALID.MAX_LENGTH }
   )
   newPassword: string;
 }
