1546a53874123deacadfe91679c04b2656c1b30c
focuzzzed <1limbo.theorem1@gmail.com>
2024-01-31 06:57:10 +0300

    feat: connect config to main, implements requestIdInterceptor

--- project/apps/api-gateway/src/main.ts
+++ project/apps/api-gateway/src/main.ts
@@ -1,19 +1,33 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { RequestIdInterceptor } from './app/interceptors/request-id.interceptor';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('The «Api-Gateway» service')
+    .setDescription('Api-Gateway service API')
+    .setVersion('1.0')
+    .build();
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('spec', app, document);
+
+  app.useGlobalInterceptors(new RequestIdInterceptor());
+
+  const configService = app.get(ConfigService);
+
+  const port = configService.get('application.port');
   await app.listen(port);
+
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
